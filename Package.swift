05526e7878ab3d7f07516d725b8c8fb69660fe03
Hassan Uriostegui <hassan@community.com>
2019-10-10 10:54:23 -0700

    update pack name

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "kron",
-            targets: ["kron"]),
+            name: "Kron",
+            targets: ["Kron"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
