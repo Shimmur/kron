a70afc7fe449d6524266db4f87f8e2f671c4720d
Hassan Uriostegui <hassan@community.com>
2019-10-10 10:51:36 -0700

    update pack name

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "kron",
+    name: "Kron",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
